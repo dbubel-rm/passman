5b308d88f86c0d84ae3cf9149d920c53b5dbfb7a
dbubel <17098360+dbubel@users.noreply.github.com>
2019-01-09 23:16:26 -0500

    added get credential route

--- models/credentials.go
+++ models/credentials.go
@@ -1,11 +1,12 @@
 package models
 
 type Credentials struct {
-	UserID      int    `json:"userId" db:"user_id"`
+	// CredentialID int       `json:"credentialId" db:"credential_id"`
+	// UserID       int       `json:"userId" db:"user_id"`
 	ServiceName string `json:"serviceName" db:"service_name"`
 	Username    string `json:"username" db:"username"`
 	Password    string `json:"password" db:"password"`
-	// UpdatedAt   time.Time `json:"updatedAt" db:"updated_at"`
-	// CreatedAt   time.Time `json:"createdAt" db:"created_at"`
-	// DeletedAt   time.Time `json:"deletedAt" db:"deleted_at"`
+	// UpdatedAt    time.Time `json:"updatedAt" db:"updated_at"`
+	// CreatedAt    time.Time `json:"createdAt" db:"created_at"`
+	// DeletedAt    time.Time `json:"deletedAt" db:"deleted_at"`
 }
