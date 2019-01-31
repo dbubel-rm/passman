c6e6213fd26bc180bc7880a0ee34277188928d18
engineerbeard <thebeard@engineerbeard.com>
2019-01-31 18:59:52 -0500

    dev stuff

---
+++ cmd/passman-api/handlers/handlers.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+// API returns a handler for a set of routes.
+func API(log *log.Logger, masterDB *db.DB, authenticator *auth.Authenticator) http.Handler {
+	app := web.New(log, mid.ErrorHandler, mid.Metrics, mid.RequestLogger)
+
+	// Register health check endpoint. This route is not authenticated.
+	check := Check{
+		MasterDB: masterDB,
+	}
+	app.Handle("GET", "/v1/health", check.Health)
+
+	// Register user management and authentication endpoints.
+	u := User{
+		MasterDB:       masterDB,
+		TokenGenerator: authenticator,
+	}
+	app.Handle("GET", "/v1/users", u.List, authmw.HasRole(auth.RoleAdmin), authmw.Authenticate)
+	app.Handle("POST", "/v1/users", u.Create, authmw.HasRole(auth.RoleAdmin), authmw.Authenticate)
+	app.Handle("GET", "/v1/users/:id", u.Retrieve, authmw.Authenticate)
+	app.Handle("PUT", "/v1/users/:id", u.Update, authmw.HasRole(auth.RoleAdmin), authmw.Authenticate)
+	app.Handle("DELETE", "/v1/users/:id", u.Delete, authmw.HasRole(auth.RoleAdmin), authmw.Authenticate)
+
+	// This route is not authenticated
+	app.Handle("GET", "/v1/users/token", u.Token)
+
+	// Register product and sale endpoints.
+	p := Product{
+		MasterDB: masterDB,
+	}
+	app.Handle("GET", "/v1/products", p.List, authmw.Authenticate)
+	app.Handle("POST", "/v1/products", p.Create, authmw.Authenticate)
+	app.Handle("GET", "/v1/products/:id", p.Retrieve, authmw.Authenticate)
+	app.Handle("PUT", "/v1/products/:id", p.Update, authmw.Authenticate)
+	app.Handle("DELETE", "/v1/products/:id", p.Delete, authmw.Authenticate)
+
+	return app
+}
---
+++ cmd/passman-cli/main.go
@@ -0,0 +1,680 @@
+package main
+
+import (
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	b64 "encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"strconv"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+type FirebaseStruct struct {
+	Kind           string `json:"kind"`
+	LocalID        string `json:"localId"`
+	Email          string `json:"email"`
+	DisplayName    string `json:"displayName"`
+	IDToken        string `json:"idToken"`
+	Registered     bool   `json:"registered"`
+	RefreshToken   string `json:"refreshToken"`
+	ExpiresIn      string `json:"expiresIn"`
+	PassmanPayload string `json:"passmanPayload"`
+}
+
+var baseUrl = "http://passman-server-alb-804849544.us-east-1.elb.amazonaws.com"
+
+var urlCreateAccount = baseUrl + "/public/user"
+var urlDeleteUser = baseUrl + "/user"
+var urlAuthUser = baseUrl + "/public/authUser"
+var urlNewCredential = baseUrl + "/credential"
+var passmanHome = "~/.passman/session.json"
+
+var argsWithoutProg = os.Args[1:]
+
+const (
+	CREATE_ACCOUNT = "create:account"
+	DELETE_ACCOUNT = "delete:account"
+	HELP           = "help"
+	VERSION        = "version"
+	GEN_PASS       = "gen:pass"
+	ADD_CRED       = "add:credential"
+	LOGIN          = "login"
+	PASSMAN_MASTER = "PASSMAN_MASTER"
+	GET_CRED       = "get:credential"
+	GET_CREDS      = "get:credentials"
+	DELETE_CRED    = "delete:credential"
+)
+
+func version() {
+	fmt.Println("v0.0.1")
+}
+
+func generateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	bytes, err := generateRandomBytes(n)
+	if err != nil {
+		return "", err
+	}
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+	return string(bytes), nil
+}
+
+func displayOptions() {
+	fmt.Println("Passman is a utility for managing your passwords.")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("\tpassman <command> [arguments]")
+	fmt.Println("")
+	fmt.Println("The commands are:")
+	fmt.Println("")
+	fmt.Printf("\t%s\t\tCreates a new passman account. Ex) passman %s newexample@example.com\n", CREATE_ACCOUNT, CREATE_ACCOUNT)
+	fmt.Printf("\t%s\t\tDeletes ALL credentials saved under you active account. Ex) passman %s\n", DELETE_ACCOUNT, DELETE_ACCOUNT)
+	fmt.Printf("\t%s\t\tAdd a credential. Ex) passman %s serviceName\n", ADD_CRED, ADD_CRED)
+	fmt.Printf("\t%s\t\tGet a stored credential. Ex) passman %s serviceName\n", GET_CRED, GET_CRED)
+	fmt.Printf("\t%s\t\tGet all stored credentials. Ex) passman %s\n", GET_CREDS, GET_CREDS)
+	fmt.Printf("\t%s\t\tDeletes a stored credential. Ex) passman %s service_name\n", DELETE_CRED, DELETE_CRED)
+	fmt.Printf("\t%s\t\tAuthenticate a passman session good for 30 minutes\n", LOGIN)
+	fmt.Printf("\t%s\t\tGenerates a crypto random string to be used for a secure password\n", GEN_PASS)
+	fmt.Printf("\t%s\t\tDisplays this message\n", HELP)
+	fmt.Printf("\t%s\t\tDisplays the version of passman\n", VERSION)
+}
+
+func main() {
+
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	passmanHome = usr.HomeDir + "/.passman/session.json"
+
+	if os.Getenv(PASSMAN_MASTER) == "" {
+		log.Println("No PASSMAN_MASTER environment vairable set")
+		return
+	}
+
+	log.SetFlags(log.Lshortfile)
+
+	actions := make(map[string]func())
+	actions[CREATE_ACCOUNT] = createAccount
+	actions[DELETE_ACCOUNT] = deleteAccount
+	actions[HELP] = displayOptions
+	actions[VERSION] = version
+	actions[GEN_PASS] = genPassword
+	actions[ADD_CRED] = addCredential
+	actions[LOGIN] = getToken
+	actions[GET_CRED] = getCredential
+	actions[GET_CREDS] = getCredentials
+	actions[DELETE_CRED] = deleteCredential
+
+	if len(argsWithoutProg) == 0 {
+		log.Println("No action specified")
+		displayOptions()
+		return
+	}
+
+	if _, err := os.Stat(passmanHome); os.IsNotExist(err) {
+		os.Create(passmanHome)
+	}
+
+	f, ok := actions[argsWithoutProg[0]]
+	if ok {
+		f()
+	} else {
+		log.Println("Invalid action specified")
+		displayOptions()
+	}
+}
+
+func getUsernameAndPassword() (string, string) {
+	fmt.Print("Username: ")
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	text = cleanInput(text)
+	fmt.Print("Password: ")
+	bytePassword, _ := terminal.ReadPassword(int(os.Stdin.Fd()))
+	password := cleanInput(string(bytePassword))
+	fmt.Println("")
+	return text, password
+}
+
+func getToken() {
+	if len(argsWithoutProg) < 2 {
+		log.Println("No account email specified")
+		return
+	}
+	username := argsWithoutProg[1]
+	password := os.Getenv(PASSMAN_MASTER)
+
+	var payload = `{"email":"%s","password":"%s",returnSecureToken: true}`
+	payload = fmt.Sprintf(payload, username, password)
+	req, err := http.NewRequest("POST", urlAuthUser, strings.NewReader(payload))
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		log.Println(string(body))
+		return
+	}
+
+	err = ioutil.WriteFile(passmanHome, body, 0644)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	log.Println("Login OK")
+}
+
+func cleanInput(s string) string {
+	s = strings.Replace(s, "\n", "", -1)
+	s = strings.Replace(s, " ", "", -1)
+	return s
+}
+
+func getUserStore() ([]byte, error) {
+	bytes, err := ioutil.ReadFile(passmanHome)
+	if err != nil {
+		return bytes, err
+	}
+	return bytes, nil
+}
+
+func genPassword() {
+	if len(argsWithoutProg) != 2 {
+		log.Println("length not specified")
+		return
+	}
+	i, err := strconv.ParseInt(argsWithoutProg[1], 10, 32)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	n, _ := GenerateRandomString(int(i))
+	fmt.Println(n)
+}
+
+func deleteAccount() {
+
+	tokenData, err := getUserStore()
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	var storedJWT FirebaseStruct
+	err = json.Unmarshal(tokenData, &storedJWT)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Println("!!! WARNING !!!")
+	log.Println("Are you sure you want to delete ALL data associaed with:", storedJWT.Email)
+	log.Println("Type: 'yes' to remove all data")
+
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	text = cleanInput(text)
+	if text != "yes" {
+		log.Println("Aborting")
+		return
+	}
+
+	req, err := http.NewRequest("DELETE", urlDeleteUser, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", storedJWT.IDToken))
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+	log.Println(string(body))
+	os.Remove(passmanHome)
+}
+
+func createAccount() {
+	payloadCreateAccount := `{"email": "%s","password": "%s",returnSecureToken: true}`
+
+	username := argsWithoutProg[1]
+	password := os.Getenv(PASSMAN_MASTER)
+
+	payloadCreateAccount = fmt.Sprintf(payloadCreateAccount, username, password)
+	req, err := http.NewRequest("POST", urlCreateAccount, strings.NewReader(payloadCreateAccount))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Println(string(body))
+		return
+	}
+
+	err = ioutil.WriteFile(passmanHome, body, 0644)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	log.Println("Account created OK")
+}
+
+func addCredential() {
+	if len(argsWithoutProg) < 2 {
+		log.Println("No service name")
+		return
+	}
+
+	newCredentialPayload := `{"serviceName": "%s","username": "%s","password": "%s"}`
+	serviceName := ""
+	tokenData, err := getUserStore()
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	var storedJWT FirebaseStruct
+	err = json.Unmarshal(tokenData, &storedJWT)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	username, password := getUsernameAndPassword()
+	fmt.Print("Confirm: ")
+
+	bytePasswordConfirm, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	passwordConfirm := cleanInput(string(bytePasswordConfirm))
+	fmt.Println()
+
+	if password != passwordConfirm {
+		fmt.Println("Passwords do not match")
+		return
+	}
+
+	password = encrypt([]byte(password), os.Getenv(PASSMAN_MASTER))
+	serviceName = argsWithoutProg[1]
+	newCredentialPayload = fmt.Sprintf(newCredentialPayload, serviceName, username, password)
+	req, err := http.NewRequest("POST", urlNewCredential, strings.NewReader(newCredentialPayload))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", storedJWT.IDToken))
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Println(string(body))
+		return
+	}
+
+	log.Println("Credential added OK")
+}
+
+func getCredential() {
+	if len(argsWithoutProg) < 2 {
+		log.Println("No service name")
+		return
+	}
+
+	serviceName := ""
+	tokenData, err := getUserStore()
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	var storedJWT FirebaseStruct
+	err = json.Unmarshal(tokenData, &storedJWT)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	serviceName = argsWithoutProg[1]
+	req, err := http.NewRequest("GET", urlNewCredential+"/"+serviceName, nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", storedJWT.IDToken))
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Println(string(body))
+		return
+	}
+
+	var credentialRecord = struct {
+		ServiceName string
+		Username    string
+		Password    string
+	}{}
+
+	err = json.Unmarshal(body, &credentialRecord)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	sDec, err := b64.StdEncoding.DecodeString(credentialRecord.Password)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	credentialRecord.Password = string(decrypt([]byte(sDec), os.Getenv(PASSMAN_MASTER)))
+	log.Println("Credential retreived OK:", credentialRecord)
+}
+
+func getCredentials() {
+	tokenData, err := getUserStore()
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	var storedJWT FirebaseStruct
+	err = json.Unmarshal(tokenData, &storedJWT)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	req, err := http.NewRequest("GET", urlNewCredential+"s", nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", storedJWT.IDToken))
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Println(string(body))
+		return
+	}
+
+	var credentialRecord = []struct {
+		ServiceName string
+		Username    string
+		Password    string
+	}{}
+
+	err = json.Unmarshal(body, &credentialRecord)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	for i := 0; i < len(credentialRecord); i++ {
+		sDec, err := b64.StdEncoding.DecodeString(credentialRecord[i].Password)
+
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		credentialRecord[i].Password = string(decrypt([]byte(sDec), os.Getenv(PASSMAN_MASTER)))
+	}
+
+	log.Println("Credential retreived OK:", credentialRecord)
+}
+
+func deleteCredential() {
+	if len(argsWithoutProg) < 2 {
+		log.Println("No service name")
+		return
+	}
+
+	serviceName := ""
+	tokenData, err := getUserStore()
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	var storedJWT FirebaseStruct
+	err = json.Unmarshal(tokenData, &storedJWT)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	serviceName = argsWithoutProg[1]
+	req, err := http.NewRequest("DELETE", urlNewCredential+"/"+serviceName, nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", storedJWT.IDToken))
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Println(string(body))
+		return
+	}
+
+	// var credentialRecord = struct {
+	// 	ServiceName string
+	// 	Username    string
+	// 	Password    string
+	// }{}
+
+	// err = json.Unmarshal(body, &credentialRecord)
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// 	return
+	// }
+
+	// sDec, err := b64.StdEncoding.DecodeString(credentialRecord.Password)
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// 	return
+	// }
+
+	// credentialRecord.Password = string(decrypt([]byte(sDec), os.Getenv(PASSMAN_MASTER)))
+	log.Println("Credential deleted OK:", string(body))
+}
+func encrypt(data []byte, passphrase string) string {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	sEnc := b64.StdEncoding.EncodeToString([]byte(ciphertext))
+	return sEnc
+}
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	gcm, err := cipher.NewGCM(block)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return plaintext
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
